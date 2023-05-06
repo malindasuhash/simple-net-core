5ddcee7c40d9f853988306882a10601ff7183dc2
malindasuhash <malinda.suhash@gmail.com>
2023-05-06 00:57:54 -0700

    End of chapter 2

---
+++ FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers;
+
+public class HomeController : Controller
+{
+    public ViewResult Index()
+    {
+        var hour = DateTime.Now.Hour;
+
+        var viewModel = hour < 12 ? "Good morning" : "Good afternoon";
+
+        return View("MyView", viewModel);
+    }
+}
